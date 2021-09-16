a3631ea348f3deaf07c62ea9fd1f164b90d37bba
MatheusHMaciel <matheusmaciel.revenda@gmail.com>
2021-09-15 21:56:59 -0300

    sistemadecontagem

---
+++ sistemadecontagem.js
@@ -0,0 +1,10 @@
+for (let i=0;i<=9;i++){
+    if (i==0){
+    console.log("Zero => ",i)
+    } else if  (i%2==0){
+        console.log("Par => ",i )
+    } else {
+        console.log("impar => ",i)
+       
+    }
+}    
